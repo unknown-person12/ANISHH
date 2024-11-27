14f6b7846bdfc1de1aca65699a9dfc8bc9447eea
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-27 16:18:02 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'moyemoye43200@gmail.com', // Replace with your Mega email
-    password: 'SILENTLOVER432#', // Replace with your Mega password
+    email: 'sadiyamin7@gmail.com', // Replace with your Mega email
+    password: 'mega@2006', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
